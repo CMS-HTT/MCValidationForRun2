11dc6c8bfce53467be3b29551c495f005a5022c9
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2015-03-24 18:54:38 +0100

    save before going home

--- MCProduction/AZhToTauTau/plotting/genAnalizer.py
+++ MCProduction/AZhToTauTau/plotting/genAnalizer.py
@@ -3,6 +3,9 @@
 import ROOT
 import sys
 from DataFormats.FWLite import Events, Handle
+from style import PlotStyle
+
+PlotStyle()
 
 # Make VarParsing object
 # https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideAboutPythonConfigFile#VarParsing_Example
@@ -17,8 +20,31 @@ options.parseArguments()
 
 # use Varparsing object
 # events = Events (options)
-events = Events (['260/HIG-RunIIWinter15GS-00003-primi-eventi.root',
-                  '260/HIG-RunIIWinter15GS-00003-primi-eventi-2.root'])
+events = Events (['../260/stuff/HIG-RunIIWinter15GS-00003-primi-eventi.root',
+                  '../260/stuff/HIG-RunIIWinter15GS-00003-primi-eventi-2.root',
+                  '../260/stuff/HIG-RunIIWinter15GS-00003-new.root',
+                  '../260/stuff/HIG-RunIIWinter15GS-00003-new2.root',
+                  '../260/stuff/prod_test2_1of20/HIG-RunIIWinter15GS-00003_1of20.root',
+                  '../260/stuff/prod_test2_2of20/HIG-RunIIWinter15GS-00003_2of20.root',
+                  '../260/stuff/prod_test2_3of20/HIG-RunIIWinter15GS-00003_3of20.root',
+                  #'260/stuff/prod_test2_4of20/HIG-RunIIWinter15GS-00003_4of20.root',
+                  '../260/stuff/prod_test2_5of20/HIG-RunIIWinter15GS-00003_5of20.root',
+                  '../260/stuff/prod_test2_6of20/HIG-RunIIWinter15GS-00003_6of20.root',
+                  #'260/stuff/prod_test2_7of20/HIG-RunIIWinter15GS-00003_7of20.root',
+                  #'260/stuff/prod_test2_8of20/HIG-RunIIWinter15GS-00003_8of20.root',
+                  '../260/stuff/prod_test2_9of20/HIG-RunIIWinter15GS-00003_9of20.root',
+                  '../260/stuff/prod_test2_10of20/HIG-RunIIWinter15GS-00003_10of20.root',
+                  '../260/stuff/prod_test2_11of20/HIG-RunIIWinter15GS-00003_11of20.root',
+                  '../260/stuff/prod_test2_12of20/HIG-RunIIWinter15GS-00003_12of20.root',
+                  '../260/stuff/prod_test2_13of20/HIG-RunIIWinter15GS-00003_13of20.root',
+                  '../260/stuff/prod_test2_14of20/HIG-RunIIWinter15GS-00003_14of20.root',
+                  '../260/stuff/prod_test2_15of20/HIG-RunIIWinter15GS-00003_15of20.root',
+                  '../260/stuff/prod_test2_16of20/HIG-RunIIWinter15GS-00003_16of20.root',
+                  '../260/stuff/prod_test2_17of20/HIG-RunIIWinter15GS-00003_17of20.root',
+                  '../260/stuff/prod_test2_18of20/HIG-RunIIWinter15GS-00003_18of20.root',
+                  '../260/stuff/prod_test2_19of20/HIG-RunIIWinter15GS-00003_19of20.root',
+                  '../260/stuff/prod_test2_20of20/HIG-RunIIWinter15GS-00003_20of20.root',
+                  ])
 
 # create handle outside of loop
 handle  = Handle ('std::vector<reco::GenParticle>')
@@ -28,9 +54,9 @@ label = ('genParticles')
 ROOT.gROOT.SetBatch()
 ROOT.gROOT.SetStyle('Plain')
 h1_pdgid = ROOT.TH1F ('h1_pdgid', 'PDG ID'  , 100, - 50,  50)
-h1_massA = ROOT.TH1F ('h1_massA', 'h1_massA',  20,  250, 270)
+h1_massA = ROOT.TH1F ('h1_massA', 'h1_massA',  20,  258, 262)
 h1_massZ = ROOT.TH1F ('h1_massZ', 'h1_massZ',  20,   85,  99)
-h1_massh = ROOT.TH1F ('h1_massh', 'h1_massh',  20,  123, 127)
+h1_massh = ROOT.TH1F ('h1_massh', 'h1_massh',  20,  124.5, 125.5)
 
 for i, event in enumerate(events):
     print i,']'
--- MCProduction/AZhToTauTau/plotting/histograms.py
+++ MCProduction/AZhToTauTau/plotting/histograms.py
@@ -1,4 +1,54 @@
+from ROOT import TH1F
+
 histograms = {}
 
-histogram['h1_massA'] = {'histo':ROOT.TH1F ('h1_massA', 'h1_massA',  20,  250, 270), '':}
+histograms['h1_A_mass'     ] = TH1F('h1_A_mass'     , '',  20,  250, 270), 'm_{A} [GeV]'
+histograms['h1_A_pt'       ] = TH1F('h1_A_pt'       , '',  20,  250, 270), 'p_{T}^{A} [GeV]'
+histograms['h1_A_eta'      ] = TH1F('h1_A_eta'      , '',  20,  250, 270), '#eta_{A} [GeV]'
+histograms['h1_A_phi'      ] = TH1F('h1_A_phi'      , '',  20,  250, 270), '#phi_{A} [GeV]'
+
+histograms['h1_Z_mass'     ] = TH1F('h1_Z_mass'     , '',  20,  250, 270), 'm_{Z} [GeV]'
+histograms['h1_Z_pt'       ] = TH1F('h1_Z_pt'       , '',  20,  250, 270), 'p_{T}^{Z} [GeV]'
+histograms['h1_Z_eta'      ] = TH1F('h1_Z_eta'      , '',  20,  250, 270), '#eta_{Z} [GeV]'
+histograms['h1_Z_phi'      ] = TH1F('h1_Z_phi'      , '',  20,  250, 270), '#phi_{Z} [GeV]'
+
+histograms['h1_h_mass'     ] = TH1F('h1_h_mass'     , '',  20,  250, 270), 'm_{h} [GeV]'
+histograms['h1_h_pt'       ] = TH1F('h1_h_pt'       , '',  20,  250, 270), 'p_{T}^{h} [GeV]'
+histograms['h1_h_eta'      ] = TH1F('h1_h_eta'      , '',  20,  250, 270), '#eta_{h} [GeV]'
+histograms['h1_h_phi'      ] = TH1F('h1_h_phi'      , '',  20,  250, 270), '#phi_{h} [GeV]'
+
+histograms['h1_channel'    ] = TH1F('h1_channel'    , '',  20,  250, 270), 'm_{A} [GeV]'
+histograms['h1_njets'      ] = TH1F('h1_njets'      , '',  20,  250, 270), 'm_{A} [GeV]'
+
+histograms['h1_tt_tau1_pt' ] = TH1F('h1_tt_tau1_pt' , '',  20,  250, 270), 'p_{T}^{#tau_{1}} [GeV]'
+histograms['h1_tt_tau1_eta'] = TH1F('h1_tt_tau1_eta', '',  20,  250, 270), '#eta_{#tau_{1}} [GeV]'
+histograms['h1_tt_tau1_phi'] = TH1F('h1_tt_tau1_phi', '',  20,  250, 270), '#phi_{#tau_{1}} [GeV]'
+histograms['h1_tt_tau2_pt' ] = TH1F('h1_tt_tau2_pt' , '',  20,  250, 270), 'p_{T}^{#tau_{2}} [GeV]'
+histograms['h1_tt_tau2_eta'] = TH1F('h1_tt_tau2_eta', '',  20,  250, 270), '#eta_{#tau_{2}} [GeV]'
+histograms['h1_tt_tau2_phi'] = TH1F('h1_tt_tau2_phi', '',  20,  250, 270), '#phi_{#tau_{2}} [GeV]'
+
+histograms['h1_mt_mu_pt'   ] = TH1F('h1_mt_mu_pt'   , '',  20,  250, 270), 'p_{T}^{#mu} [GeV]'
+histograms['h1_mt_mu_eta'  ] = TH1F('h1_mt_mu_eta'  , '',  20,  250, 270), '#eta_{#mu} [GeV]'
+histograms['h1_mt_mu_phi'  ] = TH1F('h1_mt_mu_phi'  , '',  20,  250, 270), '#phi_{#mu} [GeV]'
+histograms['h1_mt_tau_pt'  ] = TH1F('h1_mt_tau_pt'  , '',  20,  250, 270), 'p_{T}^{#tau} [GeV]'
+histograms['h1_mt_tau_eta' ] = TH1F('h1_mt_tau_eta' , '',  20,  250, 270), '#eta_{#tau} [GeV]'
+histograms['h1_mt_tau_phi' ] = TH1F('h1_mt_tau_phi' , '',  20,  250, 270), '#phi_{#tau} [GeV]'
+
+histograms['h1_et_ele_pt'  ] = TH1F('h1_et_ele_pt'  , '',  20,  250, 270), 'p_{T}^{e} [GeV]'
+histograms['h1_et_ele_eta' ] = TH1F('h1_et_ele_eta' , '',  20,  250, 270), '#eta_{e} [GeV]'
+histograms['h1_et_ele_phi' ] = TH1F('h1_et_ele_phi' , '',  20,  250, 270), '#phi_{e} [GeV]'
+histograms['h1_et_tau_pt'  ] = TH1F('h1_et_tau_pt'  , '',  20,  250, 270), 'p_{T}^{#tau} [GeV]'
+histograms['h1_et_tau_eta' ] = TH1F('h1_et_tau_eta' , '',  20,  250, 270), '#eta_{#tau} [GeV]'
+histograms['h1_et_tau_phi' ] = TH1F('h1_et_tau_phi' , '',  20,  250, 270), '#phi_{#tau} [GeV]'
+
+histograms['h1_jet1_pt'    ] = TH1F('h1_jet1_pt'    , '',  20,  250, 270), 'p_{T}^{h} [GeV]'
+histograms['h1_jet1_eta'   ] = TH1F('h1_jet1_eta'   , '',  20,  250, 270), '#eta_{h} [GeV]'
+histograms['h1_jet1_phi'   ] = TH1F('h1_jet1_phi'   , '',  20,  250, 270), '#phi_{h} [GeV]'
+histograms['h1_jet2_pt'    ] = TH1F('h1_jet2_pt'    , '',  20,  250, 270), 'p_{T}^{h} [GeV]'
+histograms['h1_jet2_eta'   ] = TH1F('h1_jet2_eta'   , '',  20,  250, 270), '#eta_{h} [GeV]'
+histograms['h1_jet2_phi'   ] = TH1F('h1_jet2_phi'   , '',  20,  250, 270), '#phi_{h} [GeV]'
+
+for k, v in histograms:
+    v.GetYaxis().SetTitle('events')
+    v.GetXaxis().SetTitle('events')
 
