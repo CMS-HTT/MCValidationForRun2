8a43af47fd62de402c2c5d26345eeeb29b3bf82c
Yuta <Yuta.Takahashi@cern.ch>
2015-08-13 15:09:39 +0200

    add scales

---
+++ scales/script.sh
@@ -0,0 +1,21 @@
+python plotscale.py --mass 800 --tanb 10 --particle A
+python plotscale.py --mass 800 --tanb 20 --particle A
+python plotscale.py --mass 800 --tanb 30 --particle A
+
+python plotscale.py --mass 200 --tanb 10 --particle A
+
+python plotscale.py --mass 400 --tanb 10 --particle A
+
+python plotscale.py --mass 600 --tanb 10 --particle A
+python plotscale.py --mass 600 --tanb 20 --particle A
+
+#python plotscale.py --mass 800 --tanb 10 --particle H
+#python plotscale.py --mass 800 --tanb 20 --particle H
+#python plotscale.py --mass 800 --tanb 30 --particle H
+#
+#python plotscale.py --mass 200 --tanb 10 --particle H
+#
+#python plotscale.py --mass 400 --tanb 10 --particle H
+#
+#python plotscale.py --mass 600 --tanb 10 --particle H
+#python plotscale.py --mass 600 --tanb 20 --particle H
